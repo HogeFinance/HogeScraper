ad8aaa581ad5ba90a6b8f6d4ef6b02c4b37c5819
Brian Hewitt <durendal@durendals-domain.com>
2021-04-20 00:40:55 -0600

    Formatting

--- get_addrs.py
+++ get_addrs.py
@@ -19,7 +19,7 @@ def get_address(blocks, addrs, scraper, lock):
 			addrs.put(tx['args']['from'])
 
 		with lock:
-			sys.stdout.write('\rBlock #%d had %d tx\'s' % (block, len(txs)))
+			sys.stdout.write('\rBlocks #%d-%d had %d tx\'s' % (block, block+1000, len(txs)))
 			sys.stdout.flush()
 
 		blocks.task_done()
@@ -29,7 +29,7 @@ def main():
 	oLock = Lock()
 	blocks = Queue(maxsize=0)
 	addrs = Queue(maxsize=0)
-	scraper = HogeScraper('INFURA_API_KEY')
+	scraper = HogeScraper('d557fda7eecd49b1861f860a9a45f4a5')
 	current = scraper.network('eth').w3().eth.getBlock('latest')['number'] # Current block number
 	deployed_at = 11809212 # Block Hoge was deployed at
 
@@ -46,7 +46,7 @@ def main():
 
 	addresses = set([addrs.get() for i in range(addrs.qsize())])
 	#print(addresses)
-	print("Found %d unique addresses holding Hoge" % len(addresses))
+	print("\nFound %d unique addresses holding Hoge" % len(addresses))
 
 if __name__ == '__main__':
 	main()
