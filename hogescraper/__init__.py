83bd5f7c72896634825f4025c23db29caf4ebb0f
Brian Hewitt <durendal@durendals-domain.com>
2021-04-21 22:14:06 -0600

    Added prices to __init__.py

--- hogescraper/__init__.py
+++ hogescraper/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["HogeScraper", "Chain", "providers", "contracts"]
+__all__ = ["HogeScraper", "Chain", "providers", "contracts", "prices"]
 
 from .HogeScraper import HogeScraper
 from .Chain import Chain
 from .contracts import Contract, ERC20, ERC721
 from .providers import Provider, Infura, XDai, Local
+from .prices import Price, CoinGecko
